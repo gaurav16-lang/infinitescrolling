a06a31214f1fd5afacf95da4c9b3f8978f07f895
Gaurav Maihuria <maihuriagaurav16@gmail.com>
2022-02-18 21:28:06 +0530

    completed

---
+++ index.js
@@ -0,0 +1,28 @@
+let ul = document.getElementById('list_wrapper')
+let page = 1
+let limit = 25
+
+function getData(page, limit) {
+  fetch(`http://localhost:3000/star?_page=${page}&_limit=${limit}`)
+    .then((res) => res.json())
+    .then((res) => {
+      res.map((item, i) => {
+        let li = document.createElement('li')
+        li.innerHTML = item.name
+        ul.appendChild(li)
+      })
+    })
+    .catch((err) => {
+      console.log('Error in fetching data', err)
+    })
+}
+
+getData(page, limit)
+
+const myDiv = document.getElementById('wrapper')
+
+myDiv.addEventListener('scroll', () => {
+  if (myDiv.offsetHeight + myDiv.scrollTop >= myDiv.scrollHeight) {
+    getData(++page, limit)
+  }
+})
